4a9b5ff6fdf6ce86cfe642064d3e0116d6430ceb
Andres Villante <andres.villante@gmail.com>
2023-07-20 20:34:01 -0300

    cambios de estilos en tarifas

--- js/tarifas.js
+++ js/tarifas.js
@@ -1,57 +1,74 @@
-// JavaScript para tarifas.html
-
-// Variable para almacenar el botón del mes previamente abierto
 let mesAnterior;
 
-// Función para mostrar la información del mes correspondiente al hacer clic en el botón
 function mostrarInformacion(mes) {
     var precioContainer = document.getElementById("precioContainer");
     var descripcionMes = document.getElementById("descripcionMes");
     var precioInfo = document.getElementById("precioInfo");
 
-    // Si el mes previo es diferente al mes actual, limpiar el contenido anterior
     if (mesAnterior !== mes) {
         precioInfo.innerHTML = "";
 
-        // Aquí puedes agregar la lógica para obtener y mostrar los precios del mes seleccionado
-        // Por ahora, solo mostramos un mensaje de ejemplo
-        precioInfo.innerHTML = "Información de precios para " + mes;
-
-        // Aquí puedes agregar la lógica para obtener y mostrar la descripción del mes seleccionado
-        // Por ahora, solo mostramos un mensaje de ejemplo
         switch (mes) {
             case "agosto":
-                descripcionMes.textContent = "Descripción de Agosto: Aquí va la descripción de agosto.";
-                break;
             case "septiembre":
-                descripcionMes.textContent = "Descripción de Septiembre: Aquí va la descripción de septiembre.";
+                descripcionMes.innerHTML = "<h3>Información de precios para agosto y septiembre</h3>" +
+                    "<p>Tarifas por noche (mínimo 2 noches):</p>" +
+                    "<p>Plan Parejas (depto. 2 amb.): $20.000</p>" +
+                    "<p>Departamento 2 ambientes: $25.000</p>" +
+                    "<p>Departamento 3 ambientes: $30.000</p>" +
+                    "<p>Departamento 4 ambientes: $35.000</p>";
                 break;
-            // Agregar los casos para los demás meses aquí
+
+            case "octubre":
+            case "noviembre":
+                descripcionMes.innerHTML = "<h3>Información de precios para octubre y noviembre</h3>" +
+                    "<p>Tarifas por noche (mínimo 2 noches):</p>" +
+                    "<p>Plan Parejas (depto. 2 amb.): $25.000</p>" +
+                    "<p>Departamento 2 ambientes: $30.000</p>" +
+                    "<p>Departamento 3 ambientes: $35.000</p>" +
+                    "<p>Departamento 4 ambientes: $40.000</p>";
+                break;
+
+            case "noviembre":
+                descripcionMes.textContent = "Descripción de Noviembre: Aquí va la descripción de noviembre.";
+                break;
+
+            case "diciembre":
+                descripcionMes.textContent = "Las tarifas para ALTA TEMPORADA estarán disponibles a partir del mes de septiembre con nuestro acostumbrado plan de cuotas.";
+                break;
+
+            case "enero":
+                descripcionMes.textContent = "Las tarifas para ALTA TEMPORADA estarán disponibles a partir del mes de septiembre con nuestro acostumbrado plan de cuotas.";
+                break;
+
+            case "febrero":
+                descripcionMes.textContent = "Las tarifas para ALTA TEMPORADA estarán disponibles a partir del mes de septiembre.";
+                break;
+
+            case "marzo":
+                descripcionMes.textContent = "Las tarifas para ALTA TEMPORADA estarán disponibles a partir del mes de septiembre con nuestro acostumbrado plan de cuotas.";
+                break;
+
             default:
                 descripcionMes.textContent = "Descripción de Mes no especificado.";
         }
 
-        // Mostrar el contenedor de precios
         precioContainer.style.display = "block";
     }
 
-    // Guardar el mes actual como el mes anterior
     mesAnterior = mes;
 }
 
-// Función para cerrar la información del mes al hacer clic en el botón "Cerrar Pregunta"
 function cerrarInformacion() {
     var precioContainer = document.getElementById("precioContainer");
     precioContainer.style.display = "none";
 }
 
-// Función para cerrar la respuesta al hacer clic en el botón "Cerrar Pregunta"
 function cerrarRespuesta() {
     var precioContainer = document.getElementById("precioContainer");
     precioContainer.style.display = "none";
 }
 
-// Ocultar el contenedor de precios al cargar la página
 window.onload = function () {
     var precioContainer = document.getElementById("precioContainer");
     precioContainer.style.display = "none";
