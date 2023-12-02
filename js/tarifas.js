5e466382db658ea3add2417c4218de0bc9443824
Andres Villante <andres.villante@gmail.com>
2023-12-02 12:17:42 -0300

    Sin precios en tarifas

--- js/tarifas.js
+++ js/tarifas.js
@@ -1,42 +1,15 @@
 const precios = {
-    agosto: {
-        parejas: "$20.000",
-        departamento2: "$25.000",
-        departamento3: "$30.000",
-        departamento4: "$35.000",
-    },
-    septiembre: {
-        parejas: "$25.000",
-        departamento2: "$30.000",
-        departamento3: "$35.000",
-        departamento4: "$40.000",
-    },
-    octubre: {
-        parejas: "$30.000",
-        departamento2: "$35.000",
-        departamento3: "$40.000",
-        departamento4: "$45.000",
-    },
-    noviembre: {
-        parejas: "$30.000",
-        departamento2: "$35.000",
-        departamento3: "$40.000",
-        departamento4: "$45.000",
-    },
     diciembre: {
-        parejas: "$35.000",
-        departamento2: "$45.000",
-        departamento3: "$55.000",
-        departamento4: "$60.000",
+        mensaje: "Consultar precios por WhatsApp."
     },
     enero: {
-        mensaje: "Las tarifas para alta temporada estarán disponibles a partir del mes de septiembre, con planes de cuotas para que tus vacaciones resulten mas accesibles."
+        mensaje: "Consultar precios por WhatsApp."
     },
     febrero: {
-        mensaje: "Las tarifas para alta temporada estarán disponibles a partir del mes de septiembre, con planes de cuotas para que tus vacaciones resulten mas accesibles."
+        mensaje: "Consultar precios por WhatsApp."
     },
     marzo: {
-        mensaje: "Las tarifas para alta temporada estarán disponibles a partir del mes de septiembre, con planes de cuotas para que tus vacaciones resulten mas accesibles."
+        mensaje: "Consultar precios por WhatsApp."
     }
 };
 
